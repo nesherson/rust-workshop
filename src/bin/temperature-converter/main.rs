36356bd7e8009208172331003924a445ee51eff4
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-03-23 23:52:28 +0100

    Add option to repeat conversion

--- src/bin/temperature-converter/main.rs
+++ src/bin/temperature-converter/main.rs
@@ -2,6 +2,7 @@ use std::io;
 fn main() {
     loop {
         let mut temperature_fahrenheit = String::new();
+        let mut repeat_conversion = String::new();
 
         println!("Please enter temperature in Fahrenheit: ");
 
@@ -19,6 +20,21 @@ fn main() {
         let temperature_celsius = (temperature_fahrenheit - 32.0) * 5.0/9.0;
 
         println!("The temperature in celsius is {}", temperature_celsius);
-        break;
+        println!("Do you want to do conversion again? (Y/N)");
+
+        io::stdin()
+            .read_line(&mut repeat_conversion)
+            .expect("Failed to read line.");
+
+        let repeat_conversion: char = repeat_conversion
+            .to_lowercase()
+            .trim()
+            .parse()
+            .expect("Failed to parse string.");
+
+        if repeat_conversion == 'n' {
+            println!("Exiting program.");
+            break;
+        }
     }
 }
