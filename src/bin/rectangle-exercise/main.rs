34a359aab1ff4b3350114719d9d9ebe2fc40eda8
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-03-26 23:43:45 +0100

    Add struct example

--- src/bin/rectangle-exercise/main.rs
+++ src/bin/rectangle-exercise/main.rs
@@ -1,6 +1,12 @@
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
 fn main() {
     simple_variable_example();
     tuple_example();
+    struct_example();
 }
 
 fn simple_variable_example() {
@@ -18,10 +24,24 @@ fn tuple_example() {
     println!("Area of rectangle is: {}\n", tuple_calculate_area(rect));
 }
 
+fn struct_example() {
+    let rect = Rectangle {
+        width: 40,
+        height: 20
+    };
+
+    println!("***Struct example***");
+    println!("Area of rectangle is: {}\n", struct_calculate_area(&rect));
+}
+
 fn simple_calculate_area(width: u32, height: u32) -> u32 {
     width * height
 }
 
 fn tuple_calculate_area(rect: (u32, u32)) -> u32 {
     rect.0 * rect.1
+}
+
+fn struct_calculate_area(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
 }
