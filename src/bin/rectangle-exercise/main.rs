6ba51735433154752c4d42f735ad85f0f0322ac4
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-03-26 16:07:53 +0100

    Add tuple example

--- src/bin/rectangle-exercise/main.rs
+++ src/bin/rectangle-exercise/main.rs
@@ -1,10 +1,27 @@
 fn main() {
+    simple_variable_example();
+    tuple_example();
+}
+
+fn simple_variable_example() {
     let width = 40;
     let height = 20;
 
-    println!("Area of rectangle is: {}", calculate_area(width, height));
+    println!("***Simple variable example***");
+    println!("Area of rectangle is: {}\n", simple_calculate_area(width, height));
 }
 
-fn calculate_area(width: u32, height: u32) -> u32 {
+fn tuple_example() {
+    let rect = (40, 20);
+
+    println!("***Tuple example***");
+    println!("Area of rectangle is: {}\n", tuple_calculate_area(rect));
+}
+
+fn simple_calculate_area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn tuple_calculate_area(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
 }
