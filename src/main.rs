3d06b72ca6dc9f28cfb5db85e473c50866e702bc
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-03-28 23:52:41 +0100

    Remove redundant code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-fn main() {
-    let mut s1 = String::from("Hello, world!");
-
-    take_ownership(&mut s1);
-
-    println!("After take_ownership {s1}");
-
-}
-
-fn take_ownership(s: &mut String) {
-    s.push_str("test 123!!!");
-}
+fn main() {}
