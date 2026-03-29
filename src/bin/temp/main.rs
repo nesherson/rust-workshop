f535a6c9f3a50e1a505638268b91c8f135c185ac
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-03-29 23:10:09 +0200

    Update exercise

---
+++ src/bin/temp/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+
+fn main() {
+    let mut first_number = String::new();
+    let mut second_number = String::new();
+    let mut operation = String::new();
+
+    println!("***Welcome to simple calculator***");
+    println!("Enter first number:");
+
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("Failed to read line");
+
+    println!("Enter second number:");
+
+    io::stdin()
+        .read_line(&mut second_number)
+        .expect("Failed to read line");
+
+    println!("Enter operation:");
+
+    io::stdin()
+        .read_line(&mut operation)
+        .expect("Failed to read line");
+
+    let first_number: u32 = first_number.trim().parse().expect("Wrong value");
+    let second_number: u32 = second_number.trim().parse().expect("Wrong value");
+
+    let result: u32;
+
+    if operation.trim() == "+" {
+        result = first_number + second_number;
+    } else {
+        result = 0
+    }
+
+    println!("Your input {result}")
+}
